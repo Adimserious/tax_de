c4214a6a75316a7d0089fe19b145b1d0f17bc86b
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-03-31 09:54:49 +0000

    Import datetime and create calculate_ovulation function

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+avarage_circle = 28
+avarage_length = 4
+
+def calculate_ovulation():
+    """"""
+    print("what was the first day of your last period?")
+    last_period = input("enter last period")
+    print(f"The date you entered is {last_period}")
+
+    print("what is your avarage cycle length?")
+    avarage_length = input("enter cycle length")
+    print(f"The number you entered is {avarage_length}")
+    
