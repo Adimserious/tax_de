5c8a4a08d0f5b9c55bd5c543c306d224713df9df
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-03-31 10:05:30 +0000

    Add docstring to calculate_ovulation function

--- run.py
+++ run.py
@@ -6,12 +6,14 @@ avarage_circle = 28
 avarage_length = 4
 
 def calculate_ovulation():
-    """"""
-    print("what was the first day of your last period?")
-    last_period = input("enter last period")
+    """period data input from user"""
+    print("what was the first day of your last period?\n")
+    last_period = input("enter last period: ")
     print(f"The date you entered is {last_period}")
 
-    print("what is your avarage cycle length?")
-    avarage_length = input("enter cycle length")
+    print("what is your avarage cycle length?\n")
+    avarage_length = input("enter cycle length: ")
     print(f"The number you entered is {avarage_length}")
     
+
+calculate_ovulation()
