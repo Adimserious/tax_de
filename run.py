3575abde573ab15247f1952cc12917393ca4666d
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-04-01 22:39:34 +0000

    Add calculate_fertility, age_validation and marriage_validation functions

--- run.py
+++ run.py
@@ -1,54 +1,57 @@
-import datetime
+import datetime as dtt
+
 from datetime import date
 
-x = date.today()
-print(x)
+age = 60
 
-avarage_circle = 28
-avarage_length = 4
 
-def calculate_ovulation():
-    """period data input from user"""
+def calculate_fertility():
+    """Age data input from user"""
 
-    print("what was the first day of your last period?")
-    print("Data should be day, month and year, seperated by dots.")
-    print("Example: 2024.04.02\n")
+    print("how old are you?")
+    print("Data should be number.")
+    print("Example: 18\n")
 
-    last_period = input("enter last period: ")
-    print(f"The date you entered is {last_period}")
+    current_age = input("enter age: ")
+    print(f"The number you entered is {current_age}")
     
     
-    period_validation(last_period)
+    age_validation(current_age)
 
-    print("what is your avarage cycle length?\n")
-    circle_length = input("enter cycle length: ")
-    print(f"The number you entered is {circle_length}")
-
-    circle_validation(circle_length)
+    print("are you married?")
+    print("yes or no?.\n" )
     
 
-def period_validation(values):
+    marital_status = input("enter status: ")
+    
+    print(f"you entered is {marital_status}")
+
+    marriage_validation(marital_status)
+    
+
+def age_validation(values):
     
     """"""
     try:
-        if len(values) != 10:
+        [int(valu) for valu in values]
+        if len(values) != 2:
             raise ValueError(
-                f"values are required in form of year, month and day, you provided {len(values)}"
+                f"2 values are required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+def marriage_validation(value):
+    """"""
+
+    try:
+        if (value) != "Yes":    
+            raise ValueError(
+                f"Yes or No; you provided {value}"
             )
 
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
-
-def circle_validation(value):
-    """"""
-    try:
-        if len(value) != 1:
-            raise ValueError(
-                f"period length should be between 4 and 7, you provided {len(value)}"
-            )
-
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-
-calculate_ovulation()
+calculate_fertility()
